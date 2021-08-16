1e15bad2f4082b0be5ef7e0c2d6ab4ec7de75302
Benedikt Albertsen <be.albertsen@gmail.com>
2021-08-16 18:28:58 +0200

    finishing all the models

--- app/models/fabric.rb
+++ app/models/fabric.rb
@@ -1,3 +1,8 @@
 class Fabric < ApplicationRecord
   belongs_to :user
+  validates :length, presence: true
+  validates :content, presence: true
+  validates :quality, presence: true
+  validates :price, presence: true
+  validates :color, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :company, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :email, presence: true
+  validates :contact_person, presence: true
 end
