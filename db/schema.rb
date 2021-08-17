5eebc9e9e6a79942762c7c0b2afd86cea3b761a7
Benedikt Albertsen <be.albertsen@gmail.com>
2021-08-17 15:44:59 +0200

    fixing db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_145757) do
+ActiveRecord::Schema.define(version: 2021_08_17_124500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,17 @@ ActiveRecord::Schema.define(version: 2021_08_16_145757) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "link"
     t.index ["user_id"], name: "index_fabrics_on_user_id"
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "fabric_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["fabric_id"], name: "index_purchases_on_fabric_id"
+    t.index ["user_id"], name: "index_purchases_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +49,6 @@ ActiveRecord::Schema.define(version: 2021_08_16_145757) do
   end
 
   add_foreign_key "fabrics", "users"
+  add_foreign_key "purchases", "fabrics"
+  add_foreign_key "purchases", "users"
 end
