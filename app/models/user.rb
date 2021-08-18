4b134c098bcc6bc4e909130391fa78a3f03527aa
Mohammad Alshahwan <shahwanoo@live.com>
2021-08-18 03:50:50 +0200

    Navbar changed so it check if the user is logged in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :company, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
